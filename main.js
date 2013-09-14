dddaf043c51e51095ac8646fcadd406cfec6fa65
hangy <hangy@warsow.net>
2013-09-14 13:40:45 +0200

    Updated - the login process to reflect some changes to the login behaviour by readmore.de - some calls to newer jsdom and request versions

--- main.js
+++ main.js
@@ -21,29 +21,36 @@ function login(req, email, password, callback) {
   var u = 'http://www.readmore.de/index.php?cont=login'  
   
   request.post(u, o, function (e, r, b) {
-    if (e || !b) {
+    if (e || r.statusCode != 302 || r.headers.location != '/') {
       callback(null, null);
       return;
     }
 
-    jsdom.env({
-      html: b,
-      scripts: [
-        'http://code.jquery.com/jquery.min.js'
-      ]
-    }, function (err, window) {
-      if (err) {
-        callback(null, null);
-        return;
-      }
+    var o2 = {
+      headers: h,
+      jar: j
+    };
+    request.get(u, o2, function(e2, r2, b2) {
+      jsdom.env({
+        html: b2,
+        scripts: [
+          'http://code.jquery.com/jquery.min.js'
+        ],
+        done: function (err, window) {
+          if (err) {
+            callback(null, null);
+            return;
+          }
 
-      var $ = window.jQuery;
+          var $ = window.jQuery;
 
-      var link = $('body div.user_band a[href^="index.php?cont=profile"]').last();
-      var href = $(link).attr('href');
-      var id = url.parse(href, true).query.id;
-      var nick = $(link).text();
-      callback(id, nick);
+          var link = $('body div.user_band a[href^="index.php?cont=profile"]').last();
+          var href = $(link).attr('href');
+          var id = url.parse(href, true).query.id;
+          var nick = $(link).text();
+          callback(id, nick);
+        }
+      });
     });
   });
 }
